6530714ff41480ee5c9cccbfc043aa5fa98e0029
Raul Rodriguez <raulrobi@gmail.com>
2020-11-26 23:25:43 -0300

    Clase 69

---
+++ src/GifExpertApp.js
@@ -0,0 +1,14 @@
+import React, {Fragment} from "react";
+import PropTypes from "prop-types";
+
+// Utilizando el comando rafc crea el componente con su contenido inicializado
+const GifExpertApp = () => {
+    return (
+        <>
+            <h2>GifExpertApp</h2>
+            <hr />
+        </>
+    );
+};
+
+export default GifExpertApp;
