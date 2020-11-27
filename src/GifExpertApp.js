56fb52583a275111119a957ccd38c87b93961196
Raul Rodriguez <raulrobi@gmail.com>
2020-11-27 11:42:30 -0300

    Clase 71 - Componente AddCategory

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -1,5 +1,6 @@
 import React, {useState} from "react";
 import PropTypes from "prop-types";
+import {AddCategory} from "./components/AddCategory";
 
 // Utilizando el comando rafc crea el componente con su contenido inicializado
 const GifExpertApp = () => {
@@ -9,24 +10,26 @@ const GifExpertApp = () => {
         "Dragon Ball"
     ]);
 
-    const handleAdd = () => {
-        // Si hacemos esto está mal. Aquí lo que hace es pasar por arriba el state de categories
-        // y lo convierte en un string
-        // setCategories("Raul");
+    // const handleAdd = () => {
+    //     // Si hacemos esto está mal. Aquí lo que hace es pasar por arriba el state de categories
+    //     // y lo convierte en un string
+    //     // setCategories("Raul");
 
-        // Extrae las categorías con el operador spread dentro del array y le agrega uno nuevo
-        //Si se quiere agregar en primer lugar: ['Raul', ...categories]
-        setCategories([...categories, "Raul"]);
+    //     // Extrae las categorías con el operador spread dentro del array y le agrega uno nuevo
+    //     //Si se quiere agregar en primer lugar: ['Raul', ...categories]
+    //     setCategories([...categories, "Raul"]);
 
-        // Otra forma es retornar un callback
-        // setCategories((cats) => [...cats, "Raul"]);
-    };
+    //     // Otra forma es retornar un callback
+    //     // setCategories((cats) => [...cats, "Raul"]);
+    // };
 
     return (
         <>
             <h2>GifExpertApp</h2>
+
+            <AddCategory />
+
             <hr />
-            <button onClick={handleAdd}>Agregar</button>
             <ol>
                 {
                     // Siempre se necesita tener el key de cada elemento
---
+++ src/components/AddCategory.js
@@ -0,0 +1,11 @@
+import React, {useState} from "react";
+
+export const AddCategory = () => {
+    const [state, setstate] = useState(initialState);
+
+    return (
+        <>
+            <input />
+        </>
+    );
+};
