2fa3efcbf2fe34c62b38bf82071dabc31690daf6
Raul Rodriguez <raulrobi@gmail.com>
2020-11-28 21:26:50 -0300

    Clase 71 - Componente AddCategory

--- src/components/AddCategory.js
+++ src/components/AddCategory.js
@@ -1,11 +1,21 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 export const AddCategory = () => {
-    const [state, setstate] = useState(initialState);
+  const [inputValue, setInputValue] = useState("Hola Mundo");
 
-    return (
-        <>
-            <input />
-        </>
-    );
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    // Cuando utilizamos un elemento form, no es necesario utilizar un
+    // Fragment porque el form agrupa todos los elementos
+    <form onSubmit={handleSubmit}>
+      <input type="text" value={inputValue} onChange={handleInputChange} />
+    </form>
+  );
 };
