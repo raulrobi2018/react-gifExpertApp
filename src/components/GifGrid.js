f4c682d3ee5a83f5c21ce58fb42d3447d86d6ad8
Raul Rodriguez <raulrobi@gmail.com>
2020-11-30 09:28:14 -0300

    Clase 75 - Mostrar imagenes con título y key

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,17 +1,18 @@
 import React, {useState, useEffect} from "react";
+import GifGridItem from "./GifGridItem";
 
 export const GifGrid = ({category}) => {
-    const [count, setcount] = useState(0);
-
     // Utilizamos el useEffect para que el componente se reenderize
     // una sola vez
     useEffect(() => {
         getGifs();
     }, []);
 
+    const [images, setimages] = useState([]);
+
     const getGifs = async () => {
         const url =
-            "https://api.giphy.com/v1/gifs/search?limit=10&q=jorge&api_key=3Ysdk9Dh04shPsSSyQtka6WqJ57UNWzO";
+            "https://api.giphy.com/v1/gifs/search?limit=10&q=cat&api_key=3Ysdk9Dh04shPsSSyQtka6WqJ57UNWzO";
         const resp = await fetch(url);
 
         const {data} = await resp.json();
@@ -19,20 +20,26 @@ export const GifGrid = ({category}) => {
         const gifs = data.map((img) => {
             return {
                 id: img.id,
-                title: img.tile,
+                title: img.title,
                 // Ponemos el operador '?' para asegurarnos de que el atributo exista
                 url: img.images?.downsized_medium.url
             };
         });
 
-        console.log(gifs);
+        setimages(gifs);
     };
 
     return (
         <div>
             <h3>{category}</h3>
-            <h4>{count}</h4>
-            <button onClick={() => setcount(count + 1)}></button>
+            {images.map((img) => {
+                // Al enviar '...img' estoy enviando cada una de las propiedades de la imagen
+                // de manera independiente
+                return <GifGridItem {...img} key={img.id} />;
+
+                // Otra forma
+                // return <GifGridItem img={img} key={img.id} />;
+            })}
         </div>
     );
 };
