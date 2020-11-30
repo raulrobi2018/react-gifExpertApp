0e2af3c9ce1d87337d6beb3a9b09370b7257bec7
Raul Rodriguez <raulrobi@gmail.com>
2020-11-29 23:33:33 -0300

    Clase 74 - useEffect

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,32 +1,40 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 
-export const GifGrid = ({ category }) => {
-  const getGifs = async () => {
-    const url =
-      "https://api.giphy.com/v1/gifs/search?limit=10&q=jorge&api_key=3Ysdk9Dh04shPsSSyQtka6WqJ57UNWzO";
-    const resp = await fetch(url);
+export const GifGrid = ({category}) => {
+    const [count, setcount] = useState(0);
 
-    const { data } = await resp.json();
+    // Utilizamos el useEffect para que el componente se reenderize
+    // una sola vez
+    useEffect(() => {
+        getGifs();
+    }, []);
 
-    const gifs = data.map((img) => {
-      return {
-        id: img.id,
-        title: img.tile,
-        // Ponemos el '?' para asegurarnos de que el atributo exista
-        url: img.images?.downsized_medium.url
-      };
-    });
+    const getGifs = async () => {
+        const url =
+            "https://api.giphy.com/v1/gifs/search?limit=10&q=jorge&api_key=3Ysdk9Dh04shPsSSyQtka6WqJ57UNWzO";
+        const resp = await fetch(url);
 
-    console.log(gifs);
-  };
+        const {data} = await resp.json();
 
-  getGifs();
+        const gifs = data.map((img) => {
+            return {
+                id: img.id,
+                title: img.tile,
+                // Ponemos el operador '?' para asegurarnos de que el atributo exista
+                url: img.images?.downsized_medium.url
+            };
+        });
 
-  return (
-    <div>
-      <h3>{category}</h3>
-    </div>
-  );
+        console.log(gifs);
+    };
+
+    return (
+        <div>
+            <h3>{category}</h3>
+            <h4>{count}</h4>
+            <button onClick={() => setcount(count + 1)}></button>
+        </div>
+    );
 };
 
 export default GifGrid;
