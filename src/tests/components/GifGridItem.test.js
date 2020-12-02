b952fa9d816ec73cdb4e19501488559786495859
rrodriguez <rrodriguez@cjpb.org.uy>
2020-12-02 19:47:57 -0300

    Clase 90 - Testing en componentte GifGridItem

--- src/tests/components/GifGridItem.test.js
+++ src/tests/components/GifGridItem.test.js
@@ -1,4 +1,4 @@
-import {shallow} from "enzyme";
+import { shallow } from "enzyme";
 import GifGridItem from "../../components/GifGridItem";
 import React from "react";
 
@@ -6,11 +6,30 @@ import React from "react";
 import "@testing-library/jest-dom";
 
 describe("Testing GifGridItem", () => {
-    const title = "Title";
-    const url = "https://localhost/algo.jpg";
+            const title = "Title";
+            const url = "https://localhost/algo.jpg";
+            const wrapper = shallow( < GifGridItem title = { title }
+                url = { url }
+                />);
 
-    test("Should return GifGridItem correctly", () => {
-        const wrapper = shallow(<GifGridItem title={title} url={url} />);
-        expect(wrapper).toMatchSnapshot();
-    });
-});
+                test("Should return GifGridItem correctly", () => {
+                    expect(wrapper).toMatchSnapshot();
+                });
+
+                test('Should have a p element with the title', () => {
+                    const p = wrapper.find('p');
+                    expect(p.text().trim()).toBe(title);
+                });
+
+                test('the img element should have the url and alt attribute as the props', () => {
+                    const img = wrapper.find('img');
+                    expect(img.prop('src')).toBe(url);
+                    expect(img.prop('alt')).toBe(title);
+                });
+
+                test('should have the animate__bounce class', () => {
+                    const div = wrapper.find('div');
+                    expect(div.prop('className').includes('animate__bounce')).toBe(true);
+                });
+
+            });
