679914d3a34bd56cea796dd6b909857c657e41d4
Raul Rodriguez <raulrobi@gmail.com>
2020-12-15 07:38:40 -0300

    Class 99 - Testing custom hooks

--- src/tests/hooks/useFetchGifs.test.js
+++ src/tests/hooks/useFetchGifs.test.js
@@ -4,9 +4,14 @@ import {renderHook} from "@testing-library/react-hooks";
 describe("Testing useFetchGifs", () => {
     test("should return the initial state", () => {
         // Esta función es de testing-library y se utiliza para testear hooks
-        const {res} = renderHook(() => useFetchGifs("One Punch"));
-        const {data, loading} = res.current;
+        const {result} = renderHook(() => useFetchGifs("One Punch"));
+        const {data, loading} = result.current;
 
         console.log(data, loading);
+
+        //El arreglo de imagenes debe ser vacío
+        expect(data).toEqual([]);
+        // El loading debe ser true
+        expect(loading).toBe(true);
     });
 });
